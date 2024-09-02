2d149baae2bfe1393e6517cec77fa1a6536808e2
integerji <jsji99@golfzon.com>
2024-09-02 14:10:45 +0900

    Feat: add statis url

--- mychatbot/urls.py
+++ mychatbot/urls.py
@@ -1,6 +1,8 @@
 # project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,4 +26,4 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
